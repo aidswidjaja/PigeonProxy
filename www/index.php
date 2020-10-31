311f8ad2911c39ae12f5749fb85a709e6a5cefea
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-10-31 21:36:07 +1100

    index.php minor changes

--- www/index.php
+++ www/index.php
@@ -10,12 +10,12 @@
     <h1>PigeonProxy</h1>
     <h3>play shellshock.io with friends at school!</h3>
     <p>
-        PigeonProxy lets you play <a href="https://yolk.best">Shell Shockers</a> under any filtering service that uses the PAN-DB filtering and categorisation IP/URL database from Palo Alto Networks (which means, at school!)<br><br>
+        PigeonProxy lets you play private games of <a href="https://yolk.best">Shell Shockers</a> with your friends, under any filtering service that uses the PAN-DB filtering and categorisation IP/URL database from Palo Alto Networks (which means, at school!)<br><br>
 
         Blue Wizard Digital releases alternate URLs for Shell Shockers, and while most of them are blocked, some of them do work!! Unfortunately, there is one problematic issue - it doesn't show you the join link while playing in private lobbies... and that's where PigeonProxy comes in - it'll create a game for you to play with you and your friends!<br><br>
 
-        If you're interested in more information about PigeonProxy, visit the official project Google Doc <a href="https://docs.google.com/document/d/1UA4LpxPjmrwpQwIoFoXCzsodPXYq7hSnbY52iydH0zI/edit?usp=sharing">here</a>.<br>
-        For an end user instruction manual on how to use PigeonProxy, you'll find the official instruction manual <a href="https://docs.google.com/document/d/1-Jme2uSqgeaNkweX0GFIPHLPx5xIMq4xv8GrFBM-oSA/edit?usp=sharing">here</a>.<br><br>
+        If you're interested in the technical tidbits about PigeonProxy, visit the official project Google Doc <a href="https://docs.google.com/document/d/1UA4LpxPjmrwpQwIoFoXCzsodPXYq7hSnbY52iydH0zI/edit?usp=sharing">here</a>.<br>
+        If you're just a normal user who wants to lear how to use PigeonProxy to set up games for you and your friends, you'll find the official instruction manual <a href="https://docs.google.com/document/d/1-Jme2uSqgeaNkweX0GFIPHLPx5xIMq4xv8GrFBM-oSA/edit?usp=sharing">here</a>.<br><br>
         <p><strong class="tip">tip!</strong> we've set the tab name of this webpage to say Google Classroom to make things a bit more inconspicious! but please don't play during class :(</p>
     </p><br>
     <a href="create.php" class="button" style="background: #5bc0de">Create a game</a>
@@ -23,7 +23,7 @@
     <a href="https://docs.google.com/document/d/1-Jme2uSqgeaNkweX0GFIPHLPx5xIMq4xv8GrFBM-oSA/edit?usp=sharing" class="button" style="background: #ff5b52">How to use</a>
     <a href="https://docs.google.com/document/d/1UA4LpxPjmrwpQwIoFoXCzsodPXYq7hSnbY52iydH0zI/edit?usp=sharing" class="button" style="background: #5cb85c">Project information</a>
     <a href="https://github.com/aidswidjaja/PigeonProxy" class="button" style="background: #24292e">GitHub</a>
-    <a href="https://status.adrian.id.au" class="button" style="background: #008000">Server status</a>
+    <a href="https://status.adrian.id.au" class="button" style="background: #c887cc">Server status</a>
 
     <br><br><br><br>
 </body>
