8b8456b453e7952de6acc3337da00c36816c2302
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-02 15:46:24 +1100

    I wanted bullet points

--- www/index.php
+++ www/index.php
@@ -9,8 +9,10 @@
 
         Blue Wizard Digital releases alternate URLs for Shell Shockers, and while most of them are blocked, some of them do work!! Unfortunately, there is one problematic issue - it doesn't show you the join link while playing in private lobbies... and that's where PigeonProxy comes in - it'll create a game for you to play with you and your friends!<br><br>
 
-        If you're interested in the technical tidbits about PigeonProxy, visit the official project Google Doc <a href="https://docs.google.com/document/d/1UA4LpxPjmrwpQwIoFoXCzsodPXYq7hSnbY52iydH0zI/edit?usp=sharing">here</a>.<br>
-        If you're just a normal user who wants to lear how to use PigeonProxy to set up games for you and your friends, you'll find the official instruction manual <a href="https://docs.google.com/document/d/1-Jme2uSqgeaNkweX0GFIPHLPx5xIMq4xv8GrFBM-oSA/edit?usp=sharing">here</a>.<br>
+        <ul>
+        <li>If you're interested in the technical tidbits about PigeonProxy, visit the official project Google Doc <a href="https://docs.google.com/document/d/1UA4LpxPjmrwpQwIoFoXCzsodPXYq7hSnbY52iydH0zI/edit?usp=sharing">here</a>.</li>
+        <li>If you're just a normal user who wants to lear how to use PigeonProxy to set up games for you and your friends, you'll find the official instruction manual <a href="https://docs.google.com/document/d/1-Jme2uSqgeaNkweX0GFIPHLPx5xIMq4xv8GrFBM-oSA/edit?usp=sharing">here</a>.</li>
+        </ul>
         <p><strong class="tip">tip!</strong> we've set the tab name of this webpage to say Google Classroom to make things a bit more inconspicious! but please don't play during class :(</p>
     </p><br>
     <a href="create.php" class="button" style="background: #5bc0de">Create a game</a>
