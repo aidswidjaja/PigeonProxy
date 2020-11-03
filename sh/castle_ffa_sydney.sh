1b4082dec38add39e2cca75ac48d52ee31752a97
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-03 16:46:21 +1100

    minor changes to bash files

--- sh/castle_ffa_sydney.sh
+++ sh/castle_ffa_sydney.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo castle_ffa_sydney successfully initiated
+cd
 python3.6 /home/adrian/Desktop/castle_ffa_sydney.pyc
--- sh/castle_teams_sydney.sh
+++ sh/castle_teams_sydney.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo castle_teams_sydney successfully initiated
+cd
 python3.6 /home/adrian/Desktop/castle_teams_sydney.pyc
