b2c8099559cb2377c02a2363b68b0d53d58d17dd
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-03 15:06:53 +1100

    update.sh improvements

--- sh/update.sh
+++ sh/update.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 # by adrian
 
+echo "[update.sh] Starting..."
+echo "[update.sh] cd /var/www/pigeonproxy"
 cd /var/www/pigeonproxy
-echo "cd executed"
+echo "[update.sh] sudo git fetch"
 sudo git fetch
-echo "git fetch executed"
+echo "[update.sh] sudo git reset --hard origin/main"
 sudo git reset --hard origin/main
-echo "git reset executed"
-cd www && sudo chown 777 data.txt
-echo "cd www and chown executed"
+echo "[update.sh] cd www"
+cd www
+echo "[update.sh] sudo chown 777 data.txt"
+sudo chown 777 data.txt
+echo "[update.sh] sudo nginx -s reload"
+sudo nginx -s reload
+echo "[update.sh] end of file."
