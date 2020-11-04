bc3d2d148cec58e1fa4e8ec02abf67e9c956774b
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-04 20:44:06 +1100

    update sh files

--- sh/update.sh
+++ sh/update.sh
@@ -12,6 +12,10 @@ echo "[update.sh] cd www"
 cd www
 echo "[update.sh] sudo chmod 777 data.txt"
 sudo chmod 777 data.txt
+echo "[update.sh] sudo chmod 777 host.txt"
+sudo chmod 777 host.txt
+echo "[update.sh] sudo chmod 777 invite.txt"
+sudo chmod 777 invite.txt
 echo "[update.sh] sudo nginx -s reload"
 sudo nginx -s reload
 echo "[update.sh] end of file."
