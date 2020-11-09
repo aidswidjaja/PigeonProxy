de7374e152e0fad8f44c93dc091f8d05591eb1e5
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-09 20:21:41 +1100

    remove PATH

--- www/sh/castle_ffa_sydney.sh
+++ www/sh/castle_ffa_sydney.sh
@@ -3,5 +3,5 @@ export PATH="usr/bin/cnee:$PATH"
 PWDOUTPUT=$("pwd")
 echo "${PWDOUTPUT}"
 cd
-OUTPUT=$("cnee --replay -f castle_ffa_sydney.xnl --recall-window-position")
+OUTPUT=$("/usr/bin/cnee --replay -f castle_ffa_sydney.xnl --recall-window-position")
 echo "${OUTPUT}"
