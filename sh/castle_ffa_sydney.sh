a9518e76e0b6361a530bdb7d89380e4a796ddc9a
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-09 21:19:34 +1100

    Specify LVDS-1 as Display for CNEE

--- sh/castle_ffa_sydney.sh
+++ sh/castle_ffa_sydney.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cnee --replay -f /home/adrian/castle_ffa_sydney.xnl --recall-window-position
+cnee --replay -f /home/adrian/castle_ffa_sydney.xnl --display LVDS-1 --recall-window-position
--- sh/castle_teams_sydney.sh
+++ sh/castle_teams_sydney.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cnee --replay -f /home/adrian/castle_teams_sydney.xnl --recall-window-position
+cnee --replay -f /home/adrian/castle_teams_sydney.xnl --display LVDS-1 --recall-window-position
