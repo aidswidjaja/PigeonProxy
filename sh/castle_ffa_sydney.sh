7347169be71cf765b01b374254a697dd906d0195
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-09 11:24:47 +1100

    rm echo

--- sh/castle_ffa_sydney.sh
+++ sh/castle_ffa_sydney.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-echo castle_ffa_sydney successfully instantiated
 cd
 cnee --replay -f castle_ffa_sydney.xnl --recall-window-position
-echo castle_ffa_sydney has completed
--- sh/castle_teams_sydney.sh
+++ sh/castle_teams_sydney.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-echo castle_teams_sydney successfully instantiated
 cd
 cnee --replay -f castle_teams_sydney.xnl --recall-window-position
-echo castle_teams_sydney has completed
