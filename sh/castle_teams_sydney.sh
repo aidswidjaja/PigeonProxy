e09cf3e8687a5a441f35bf271fa614bec3895a0b
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-09 21:09:40 +1100

    bashosimplified

--- sh/castle_teams_sydney.sh
+++ sh/castle_teams_sydney.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-WHICHOUTPUT=$("git help")
-echo "${WHICHOUTPUT}"
 cd || echo "failed :("
-OUTPUT=$("cnee --replay -f castle_teams_sydney.xnl --recall-window-position")
-echo "${OUTPUT}"
+cnee --replay -f castle_ffa_sydney.xnl --recall-window-position
