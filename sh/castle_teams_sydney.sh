302ee4a6801abf9ac0d06c244a48ba07a368e635
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-09 21:05:09 +1100

    PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/bin

--- sh/castle_teams_sydney.sh
+++ sh/castle_teams_sydney.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export PATH=$PATH:/usr/bin/cnee
-cd ... || echo "failed :("
+export PATH=$PATH:/usr/bin/
+cd || echo "failed :("
 OUTPUT=$("cnee --replay -f castle_teams_sydney.xnl --recall-window-position")
 echo "${OUTPUT}"
