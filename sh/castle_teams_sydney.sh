130de89b96b9427a97e997692dbacdd62d983cbf
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-14 21:56:35 +1100

    test page + sh fixes

--- sh/castle_teams_sydney.sh
+++ sh/castle_teams_sydney.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DISPLAY=:0
-cnee --replay  --display :0  -f /home/adrian/castle_teams_sydney.xnl --recall-window-position
+echo `cnee --replay  --display :0  -f /home/adrian/castle_teams_sydney.xnl --recall-window-position`
---
+++ sh/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo this is only a test!
+echo again, this is a test!
+echo I like pineapples!
