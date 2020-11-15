4175afc994f0b2efdcb11db73da6e9893b85a105
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-15 17:12:20 +1100

    no revert all of that I don't like that

--- www/footer.php
+++ www/footer.php
@@ -2,9 +2,9 @@
 echo "
 <br>
 <footer>
-<strong>Version 0.3-alpha-rc1-ptb of PigeonProxy</strong><br> <!-- manually update versions -->
-    Copyright &copy Pigeon and Friends 2020 - MIT License<br>
-    Shell Shockers is Copyright &copy Blue Wizard Digital 2020<br>
-    A project by aidswidjaja • <a href='https://aidswidjaja.github.io'>aidswidjaja.github.io</a>
+    Version 0.3-alpha-rc1-ptb of PigeonProxy<br> <!-- manually update versions -->
+    <strong>Copyright &copy Pigeon and Friends 2020 - MIT License</strong><br>
+    <strong>Shell Shockers is Copyright &copy Blue Wizard Digital 2020</strong><br>
+    <strong>A project by aidswidjaja • <a href='https://aidswidjaja.github.io'>aidswidjaja.github.io</a></strong>
 </footer>";
 ?>
