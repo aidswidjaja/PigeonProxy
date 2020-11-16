a5ce21a04576f782bb714b5c5ef9dcb3d1e9e43e
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-16 22:30:34 +1100

    remove tsp

--- sh/castle_ffa_sydney.sh
+++ sh/castle_ffa_sydney.sh
@@ -2,7 +2,8 @@
 # note to self - remember to chmod 777
 echo "display: $DISPLAY"
 echo "now executing castle_ffa_sydney..."
-echo `tsp -c 'cnee --replay --display :0 -f /home/adrian/castle_ffa_sydney.xnl --stop-key z --recall-window-position'`
+echo `cnee --replay --display :0 -f /home/adrian/castle_ffa_sydney.xnl --stop-key z --recall-window-position`
+echo "!!! NO TSP !!!"
 echo "=== TSP JOB LIST ==="
 echo `tsp -l`
 echo "=== TSP OUTPUT OF LAST RUN ==="
--- sh/update.sh
+++ sh/update.sh
@@ -9,6 +9,11 @@ echo "[update.sh] sudo git fetch"
 sudo git fetch
 echo "[update.sh] sudo git reset --hard origin/main"
 sudo git reset --hard origin/main
+echo "[update.sh] sudo chmod 777 sh"
+sudo chmod 777 sh/castle_ffa_sydney.sh
+sudo chmod 777 sh/castle_teams_sydney.sh
+sudo chmod 777 sh/road_ffa_sydney.sh
+sudo chmod 777 sh/road_teams_sydney.sh
 echo "[update.sh] cd www"
 cd www
 echo "[update.sh] sudo chmod 777 data.txt"
