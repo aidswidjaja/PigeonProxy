0443419ca1a548e3d248d618fcc1f527eef2e413
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-16 19:49:19 +1100

    add shell script improvements

--- sh/castle_teams_sydney.sh
+++ sh/castle_teams_sydney.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "display: $DISPLAY"
+echo "now executing castle_teams_sydney..."
 echo `cnee --replay  --display :0  -f /home/adrian/castle_teams_sydney.xnl --stop-key z --recall-window-position`
