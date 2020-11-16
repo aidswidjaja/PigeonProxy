339dda2ae61b07093006d59c8f6943fa726940d9
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-16 20:49:59 +1100

    execute commands

--- sh/castle_ffa_sydney.sh
+++ sh/castle_ffa_sydney.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "display: $DISPLAY"
 echo "now executing castle_ffa_sydney..."
-echo "cnee --replay --display :0 -f /home/adrian/castle_ffa_sydney.xnl --stop-key z --recall-window-position"
+echo `cnee --replay --display :0 -f /home/adrian/castle_ffa_sydney.xnl --stop-key z --recall-window-position`
