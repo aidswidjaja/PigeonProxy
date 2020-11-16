20214fd7697405836ab77cacb51d6a1975a27087
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-16 22:33:58 +1100

    remove tsp

--- sh/road_teams_sydney.sh
+++ sh/road_teams_sydney.sh
@@ -2,7 +2,7 @@
 # note to self - remember to chmod 777
 echo "display: $DISPLAY"
 echo "now executing road_teams_sydney..."
-echo `tsp -c 'cnee --replay --display :0 -f /home/adrian/road_teams_sydney.xnl --stop-key z --recall-window-position'`
+echo `cnee --replay --display :0 -f /home/adrian/road_teams_sydney.xnl --stop-key z --recall-window-position`
 echo "=== TSP JOB LIST ==="
 echo `tsp -l`
 echo "=== TSP OUTPUT OF LAST RUN ==="
