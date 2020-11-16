6a24075e1e9f5df10760911421a662b54135ba88
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2020-11-16 20:46:50 +1100

    Remove task spooler

--- sh/road_teams_sydney.sh
+++ sh/road_teams_sydney.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "display: $DISPLAY"
 echo "now executing road_teams_sydney..."
-echo `tsp cnee --replay  --display :0  -f /home/adrian/road_teams_sydney.xnl --stop-key z --recall-window-position`
+echo "cnee --replay  --display :0  -f /home/adrian/road_teams_sydney.xnl --stop-key z --recall-window-position"
